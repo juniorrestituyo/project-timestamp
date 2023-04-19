07a24011797ca8393e25e6697811faf49fdcfd16
Roberto Jr. Restituyo <101441219+juniorrestituyo@users.noreply.github.com>
2023-04-19 15:15:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors')
+const cors = require('cors')
 app.use(cors({optionsSuccessStatus: 200}))  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -16,37 +16,54 @@ app.use(express.static('public'))
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
-});
-
+})
 
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
   try {
-    let paramsData = req.params.date
+    const paramsData = req.params.date
 
-    if (isValidDate(paramsData)) {
-      let unixDate = toTimestamp(paramsData) * 1000
-      let utcDate = new Date(unixDate).toUTCString()
+    if (!paramsData) {
+      const utcDate = new Date().toUTCString()
+      const unixDate = toTimestamp(utcDate) * 1000
+      res.json({"unix": unixDate, "utc": utcDate})
+      
+    } else if (isValidDate(paramsData)) {
+      const unixDate = toTimestamp(paramsData) * 1000
+      const utcDate = new Date(unixDate).toUTCString()
       res.json({"unix": unixDate, "utc": utcDate})
 
+    } else if (!paramsData.includes("-")) {
+      const unixDate = parseInt(paramsData)
+      const utcDate = new Date(unixDate).toUTCString()
+      res.json({"unix": unixDate, "utc": utcDate})
+      
     } else {
-      let utcDate = new Date(Number(paramsData)).toUTCString()
-      res.json({"unix": Number(paramsData), "utc": utcDate})
+      res.json({"error" : "Invalid Date"})
+      
     }
 
   } catch (err) {
-    res.json({"err" : "Invalid Date"})
+    res.json({"error" : "Invalid Date"})
+    
   }
-  
-});
+})
 
 // Validate Dates
 function isValidDate(dateString) {
-  var regEx = /^\d{4}-\d{2}-\d{2}$/
-  if(!dateString.match(regEx)) return false
-  var d = new Date(dateString)
-  var dNum = d.getTime()
-  if(!dNum && dNum !== 0) return false
+  const regEx = /^\d{4}-\d{2}-\d{2}$/
+  
+  if(!dateString.match(regEx)) {
+    return false
+  }
+  
+  const d = new Date(dateString)
+  const dNum = d.getTime()
+  
+  if(!dNum && dNum !== 0) {
+    return false
+  }
+  
   return d.toISOString().slice(0,10) === dateString
 }
 
@@ -54,10 +71,9 @@ function isValidDate(dateString) {
 function toTimestamp(strDate) {
   const dt = Date.parse(strDate)
   return dt / 1000
-};
-
+}
 
 // listen for requests :) 
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port)
-});
+})
